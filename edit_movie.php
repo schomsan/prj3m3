8f73a881cd85428a1a1d4aea9fa051a35b83edb4
Ben Chomsang <schomsan@u.rochester.edu>
2018-04-04 14:45:21 -0400

    added Profiles.php

--- edit_movie.php
+++ edit_movie.php
@@ -1,2 +1,22 @@
 <?php include 'partials/heading.php' ?>
+<div class=" container vh-full p-5">
+            <h1 class=" p-3">Edit a movie.</h1>
+            <form action="" method="post">
+                <div class="form-group text-center">
+                        <input class="form-control m-2" type="number" name="movie_id" id="movie_id" placeholder="Movie ID">
+                        <input type="text" name="title" class="form-control m-2" placeholder="Movie Title">
+                        <div class="form-group m-2"><label for="release_date">Release Date: </label>
+                        <input type="date" name="release_date" id="release_date" class="form-control"></div>
+                        <textarea name="synopsis" id="movie_synopsis" placeholder="Synopsis" class="form-control m-2"></textarea>
+                        <div class="form-group m-2">
+                            <input type="file" class="form-control-file m-2" id="poster_image" aria-describedby="fileHelp">
+                            <small id="fileHelp" class="form-text text-muted m-2">Upload your poster image for the movie here. Make sure its width is 185px.</small>
+                        </div>
+                        <button type="submit" class="btn btn-primary m-2">Submit</button>
+                </div>
+                
+
+            </form>
+
+        </div>
 <?php include 'partials/footer.php' ?>
--- profiles.php
+++ profiles.php
@@ -1,2 +1,12 @@
 <?php include 'partials/heading.php' ?>
+<div class="container vh-full p-5">
+    <ul class="list-group">
+        <li class="list-group-item"><a href="http://">User 1</a></li>
+        <li class="list-group-item"><a href="http://">User 2</a></li>
+        <li class="list-group-item"><a href="http://">User 3</a></li>
+        <li class="list-group-item"><a href="http://">User 4</a></li>
+        <li class="list-group-item"><a href="http://">User 5</a></li>
+    </ul>
+</div>
+
 <?php include 'partials/footer.php' ?>
